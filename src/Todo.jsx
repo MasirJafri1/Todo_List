94cb449d69eb49948bd7d7bad1038def5ba62721
MasirJafri1 <masirjafri1@gmail.com>
2024-10-07 17:06:04 +0530

    Initial structure

---
+++ src/Todo.jsx
@@ -0,0 +1,26 @@
+import "./Todo.css";
+const Todo = () => {
+  return (
+    <>
+      <section className="todo-container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+        <section className="form">
+          <form>
+            <div>
+              <input type="text" className="todo-input" autoComplete="off" />
+            </div>
+            <div>
+              <button type="submit" className="todo-btn">
+                Add Task
+              </button>
+            </div>
+          </form>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default Todo;
